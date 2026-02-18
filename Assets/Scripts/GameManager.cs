1d9c35bd0ed2131365066a626ab490342a0feb51
MSI\ylylk <254623649+ylylkpkp7@users.noreply.github.com>
2026-02-18 10:06:22 +0900

    GameManagerのアイテム所持変数の追加

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -31,11 +32,36 @@ public class GameManager : MonoBehaviour
     public bool isGameClear = false;//ゲームクリア判定
     public bool isGameOver = false;//ゲームオーバー判定
 
+    //ワールドマップで最後に入ったエントランスのドア番号
+    public static int curetDoorNumber = 0;
+
+    //所持アイテム　鍵の管理
+    public static int keys = 1;
+    //どのステージの鍵が入手済みかを管理
+    public static Dictionary<string, bool> keyGot; //シーン名,true/false
+
+    //所持アイテム　矢の管理
+    public static int arrows = 10;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         gameState = GameState.InGame;//ステータスをゲーム中にする
         soundPlayer = GetComponent<AudioSource>(); //AudioSourceを参照する
+
+        //keyGotが何もない状態だった時のみ初期化
+        if (keyGot == null)
+        {
+            keyGot = new Dictionary<string, bool>();
+        }
+
+        //もしも現シーン名がDictionary(keyGot)に登録されていなければ
+        if (!(keyGot.ContainsKey(SceneManager.GetActiveScene().name)))
+        {
+            //Dictionary(keyGot)に登録しておく(現シーン名、鍵の取得情報false)
+            keyGot.Add(SceneManager.GetActiveScene().name, false);
+        }
     }
 
     // Update is called once per frame
